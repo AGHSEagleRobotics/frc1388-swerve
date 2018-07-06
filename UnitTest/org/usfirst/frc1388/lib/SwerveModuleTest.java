273c30621428872916141dc786d1a8402b009eb1
Jeff Techau <jeff.techau@scientificdrilling.com>
2018-07-06 16:21:04 -0700

    Expanded the testSwerveModule() test case, as an example of how to test a large number of data points

--- UnitTest/org/usfirst/frc1388/lib/SwerveModuleTest.java
+++ UnitTest/org/usfirst/frc1388/lib/SwerveModuleTest.java
@@ -43,18 +43,26 @@ class SwerveModuleTest {
 	
 	@Test
 	void testNormalizeAngle() {
-		assertEquals(0, SwerveModule.normalizeAngle(0), "Target = 0");
-		assertEquals(0, SwerveModule.normalizeAngle(360), "Target = 360");
-		assertEquals(1, SwerveModule.normalizeAngle(1), "Target = 1");
-		assertEquals(100, SwerveModule.normalizeAngle(100), "Target = 100");
-		assertEquals(359.9, SwerveModule.normalizeAngle(359.9), "Target = 359.9");
-		assertEquals(140, SwerveModule.normalizeAngle(500), "Target = 500");
-		assertEquals(0, SwerveModule.normalizeAngle(720), "Target = 720");
-		assertEquals(180, SwerveModule.normalizeAngle(900), "Target = 900");
-		assertEquals(359, SwerveModule.normalizeAngle(-1), "Target = -1");
-		assertEquals(180, SwerveModule.normalizeAngle(-900), "Target = -900");
-		assertEquals(320, SwerveModule.normalizeAngle(-400), "Target = -400");
-		assertEquals(280, SwerveModule.normalizeAngle(1000000), "Target = 1000000");
+		assertEquals(  0,   SwerveModule.normalizeAngle(0),       "Target = 0");
+		assertEquals(  1,   SwerveModule.normalizeAngle(1),       "Target = 1");
+		assertEquals(100,   SwerveModule.normalizeAngle(100),     "Target = 100");
+		assertEquals(359.9, SwerveModule.normalizeAngle(359.9),   "Target = 359.9");
+		assertEquals(  0,   SwerveModule.normalizeAngle(360),     "Target = 360");
+		assertEquals(140,   SwerveModule.normalizeAngle(500),     "Target = 500");
+		assertEquals(  0,   SwerveModule.normalizeAngle(720),     "Target = 720");
+		assertEquals(180,   SwerveModule.normalizeAngle(900),     "Target = 900");
+		assertEquals(280,   SwerveModule.normalizeAngle(1000000), "Target = 1000000");
+		assertEquals(359,   SwerveModule.normalizeAngle(-1),      "Target = -1");
+		assertEquals(320,   SwerveModule.normalizeAngle(-400),    "Target = -400");
+		assertEquals(180,   SwerveModule.normalizeAngle(-900),    "Target = -900");
+		
+		// Sweep through many values
+		for (double angle = -1001.0; angle <= 1001.0; angle += 0.1) {
+			double normalized = angle;
+			while (normalized <    0.0) normalized += 360.0;
+			while (normalized >= 360.0) normalized -= 360.0;
+			assertEquals(normalized, SwerveModule.normalizeAngle(angle), "Target = " + angle);
+		}
 	}
 
 }
